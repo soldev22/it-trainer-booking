1c533ab69d3deaf32d2ec4a14ef171789d344bf0
Mike Catignani <mike@solutionsdeveloped.co.uk>
2025-05-22 21:37:27 +0100

    v3

--- src/app/lib/models/Bookings/Booking.ts
+++ src/app/lib/models/Bookings/Booking.ts
@@ -1,10 +1,14 @@
-// /lib/models/Booking.ts
-{
-  clientId: ObjectId,
-  courseTitle: string,
-  startDate: Date,
-  endDate: Date,
-  status: 'provisional' | 'confirmed',
-  createdAt: Date,
-  updatedAt: Date
-}
+import mongoose, { Schema, model, models } from 'mongoose';
+
+const bookingSchema = new Schema(
+  {
+    clientId: { type: mongoose.Types.ObjectId, required: true },
+    courseTitle: { type: String, required: true },
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true },
+    status: { type: String, enum: ['provisional', 'confirmed'], default: 'provisional' },
+  },
+  { timestamps: true }
+);
+
+export const Booking = models.Booking || model('Booking', bookingSchema);
