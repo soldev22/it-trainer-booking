87e9968a701463bae94e9ff63ba83da693335a43
Mike Catignani <mike@solutionsdeveloped.co.uk>
2025-05-22 21:12:39 +0100

    Initial commit

---
+++ src/app/lib/models/Bookings/Booking.ts
@@ -0,0 +1,10 @@
+// /lib/models/Booking.ts
+{
+  clientId: ObjectId,
+  courseTitle: string,
+  startDate: Date,
+  endDate: Date,
+  status: 'provisional' | 'confirmed',
+  createdAt: Date,
+  updatedAt: Date
+}
