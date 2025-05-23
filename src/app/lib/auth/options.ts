bc28fe7c7e7ff052f05d293f676c54128e33dcf2
soldev22 <mike@solutionsdeveloped.co.uk>
2025-05-23 14:32:36 +0100

    v6

---
+++ src/app/lib/auth/options.ts
@@ -0,0 +1,62 @@
+import { type NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        const res = await fetch(`${process.env.NEXTAUTH_URL}/api/users/login`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            email: credentials?.email,
+            password: credentials?.password,
+          }),
+        });
+
+        const user = await res.json();
+
+        if (res.ok && user?.email && user?.id) {
+          return {
+            id: user.id.toString(),
+            email: user.email,
+          };
+        }
+
+        return null;
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.user = {
+          id: user.id,
+          email: user.email,
+        };
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token.user && typeof token.user === 'object') {
+        session.user = token.user as {
+          id: string;
+          email: string;
+        };
+      }
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
