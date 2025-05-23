c188a0872d62d124af8cf3e940f9527b6f5fe44a
soldev22 <mike@solutionsdeveloped.co.uk>
2025-05-23 14:34:52 +0100

    V7

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../api/auth/[...nextauth]/route';
+import { authOptions } from '@/app/lib/auth/options';
 import { redirect } from 'next/navigation';
 
 export default async function DashboardPage() {
